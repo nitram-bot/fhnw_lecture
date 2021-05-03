4cfe8539c9d8670fa1cee37a2d8af3db14ad4c7c
martin <nitram.biehler@googlemail.com>
2021-05-03 17:09:23 +0200

    stacking-notebook

--- scripts/validatedstackedmeanencoder.py
+++ scripts/validatedstackedmeanencoder.py
@@ -1,58 +1,88 @@
 import pandas as pd
+import numpy as np
 from category_encoders.james_stein import JamesSteinEncoder
 from category_encoders.cat_boost import CatBoostEncoder
 from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.base import BaseEstimator
+from sklearn.base import TransformerMixin
 
 
-class ValidatedStackedMeanEncoder:
+
+class ValidatedStackedMeanEncoder(BaseEstimator, TransformerMixin):
     """
     repeated cross-validation in constructing mean-encoding
     """
-    def __init__(self, cols: list, target_encoder=JamesSteinEncoder, k_folds: int = 5, repeats: int = 3):
+    def __init__(self):
         """
         :param cols: Categorical columns
         :param encoders_names_tuple: Tuple of str with encoders
         """
-        self.cols, self.num_cols = cols, None
-        self.encoder = target_encoder
+        target_encoder = JamesSteinEncoder
+        k_folds = 5
+        n_repeats = 3
+
+        self.target_encoder = target_encoder
         
-        self.n_folds, self.n_repeats = k_folds, repeats
-        self.model_validation = RepeatedStratifiedKFold(n_splits=self.n_folds, n_repeats=self.n_repeats, random_state=0)
+        self.k_folds, self.n_repeats = k_folds, n_repeats
+        self.model_validation = RepeatedStratifiedKFold(n_splits=self.k_folds, n_repeats=self.n_repeats, random_state=0)
         self.list_of_encoders = []
+        self._fit_index = None
 
-    def fit_transform(self, X: pd.DataFrame, y: pd.Series) -> pd.DataFrame:
-        self.num_cols = [col for col in X.columns if col not in self.cols]
-
-        cols_representation = np.zeros((X.shape[0], len(self.cols)))
+    def fit(self, X: pd.DataFrame, y: pd.Series):
+        X = pd.DataFrame(X)
+        self.list_of_encoders = []
+        self._fit_index = X.index
 
         for n_fold, (train_idx, val_idx) in enumerate(self.model_validation.split(X, y)):
+            encoder = self.target_encoder(self.cols)
+            X_train = X.lic[train_idx]
+            y_train = y[train_idx]
+            encoder.fit(X_train, y_train)
+            self.list_of_encoders.append(encoder)
+
+        return self
+
+    def fit_transform(self, X, y):
+        cols_representation = np.zeros_like(X)
+        X = pd.DataFrame(X)
+        cols = X.columns
+        for n_fold, (train_idx, val_idx) in enumerate(self.model_validation.split(X, y)):
             
-            encoder = self.encoder(self.cols)
-            X_train, X_val = X.loc[train_idx].reset_index(drop=True), X.loc[val_idx].reset_index(drop=True)
+            encoder = self.target_encoder(X.columns)
+            X_train, X_val = X.loc[train_idx, :].reset_index(drop=True), X.loc[val_idx, :].reset_index(drop=True)
+            # X_train, X_val = X[train_idx], X[val_idx]
             y_train, y_val = y[train_idx], y[val_idx]
             _ = encoder.fit_transform(X_train, y_train)
 
             # transform validation part and get all necessary cols
             val_t = encoder.transform(X_val)
-            val_t = val_t[[col for col in val_t.columns if col not in self.num_cols]].values
+
 
             cols_representation[val_idx, :] += val_t / self.n_repeats
             self.list_of_encoders.append(encoder)
 
-        X = pd.concat([X, pd.DataFrame(cols_representation)], axis = 1)
-        X.drop(self.cols, axis=1, inplace=True)
+        cols_representation = pd.DataFrame(cols_representation, columns=['mean_enc_' + c for c in cols])
+        X = pd.concat([X, cols_representation], axis=1)
+        X.drop(cols, axis=1, inplace=True)
         return X
 
     def transform(self, X: pd.DataFrame) -> pd.DataFrame:
-        cols_representation = np.zeros((X.shape[0], len(self.cols)))
+        X = pd.DataFrame(X)
+        cols_representation = np.zeros_like(X)
+        cols = X.columns
+        if self._fit_index and len(self._fit_index) == len(X.index):
+            print('in if of transform')
+            for n_fold, (train_idx, val_idx) in enumerate(self.model_validation.split(X)):
+                val_t = self.list_of_encoders[n_fold].transform(X.loc[val_idx].reset_index(drop=True))
+                cols_representation[val_idx, :] += val_t / self.n_repeats
+        else:
+            print('in else of transform')
 
-        for encoder in self.list_of_encoders:
-            test_tr = encoder.transform(X)
-            test_tr = test_tr[[col for col in test_tr.columns if col not in self.num_cols]].values
+            for encoder in self.list_of_encoders:
+                test_tr = encoder.transform(X)
+                cols_representation = cols_representation + (test_tr / self.k_folds / self.n_repeats)
 
-            cols_representation = cols_representation + test_tr / self.n_folds / self.n_repeats
-
-        cols_representation = pd.DataFrame(cols_representation)
+        cols_representation = pd.DataFrame(cols_representation, columns=['mean_enc_' + c for c in cols])
         X = pd.concat([X, cols_representation], axis=1)
-        X.drop(self.cols, axis=1, inplace=True)
+        X.drop(cols, axis=1, inplace=True)
         return X
